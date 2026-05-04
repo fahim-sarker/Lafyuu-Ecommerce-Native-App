becaae77e93014f3a5c15a86487fc5d0f3a3e891
fahim-sarker <sarkerfahim599@gmail.com>
2026-05-04 15:56:57 +0600

    feat: create the signup page

--- app/authscreen/signin/index.tsx
+++ app/authscreen/signin/index.tsx
@@ -1,10 +1,22 @@
+import { AntDesign, FontAwesome, MaterialIcons } from "@expo/vector-icons";
 import { Link } from "expo-router";
-import { Text, View,Image, TextInput, TouchableOpacity } from "react-native";
-import { MaterialIcons, FontAwesome, AntDesign } from "@expo/vector-icons";
+import {
+  Image,
+  ScrollView,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
 
 export default function Index() {
   return (
-    <View className="flex-1 bg-white px-4 pt-16">
+    <ScrollView
+      className="flex-1 bg-white"
+      contentContainerClassName="px-4 pt-16 pb-10"
+      showsVerticalScrollIndicator={false}
+      keyboardShouldPersistTaps="handled"
+    >
       <Image
         source={require("../../../assets/images/Icon (1).png")}
         className="mb-4 mx-auto w-16 h-16"
@@ -12,13 +24,39 @@ export default function Index() {
       />
 
       <Text className="text-center font-poppins700 text-base text-[#223263]">
-        Welcome to Lafyuu
+        Lets Get Started
       </Text>
       <Text className="text-center font-poppins400 text-sm text-[#9098B1] mt-1">
-        Sign in to continue
+        Create an new account
       </Text>
 
       <View className="mt-7 space-y-4">
+        <View className="relative">
+          <TextInput
+            placeholder="Your Email"
+            className="pl-12 py-3 border border-[#EBF0FF] rounded-md text-[#223263] text-sm font-poppins400"
+          />
+          <MaterialIcons
+            name="email"
+            size={20}
+            color="#9098B1"
+            className="absolute left-3 top-3"
+          />
+        </View>
+
+        <View className="relative">
+          <TextInput
+            placeholder="Your Email"
+            className="pl-12 py-3 border border-[#EBF0FF] rounded-md text-[#223263] text-sm font-poppins400"
+          />
+          <MaterialIcons
+            name="email"
+            size={20}
+            color="#9098B1"
+            className="absolute left-3 top-3"
+          />
+        </View>
+
         <View className="relative">
           <TextInput
             placeholder="Your Email"
@@ -66,7 +104,12 @@ export default function Index() {
         </TouchableOpacity>
 
         <TouchableOpacity className="flex-row items-center justify-center border border-[#EBF0FF] py-3 rounded-md">
-          <FontAwesome name="facebook" size={20} color="#1877F2" className="mr-2" />
+          <FontAwesome
+            name="facebook"
+            size={20}
+            color="#1877F2"
+            className="mr-2"
+          />
           <Text className="font-poppins400 text-sm text-[#223263]">
             Login with Facebook
           </Text>
@@ -78,11 +121,11 @@ export default function Index() {
           Forgot Password?
         </Text>
         <Link href="/authscreen/signup">
-          <Text className="text-sm font-poppins600 text-[#40BFFF] cursor-pointer">
+          <Text className="text-sm font-poppins600 text-[#40BFFF]">
             Register
           </Text>
         </Link>
       </View>
-    </View>
+    </ScrollView>
   );
 }
